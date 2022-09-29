3a19726286fad596550575fe785c6b797a302768
Micah <39250044+micah4thewin@users.noreply.github.com>
2022-09-28 22:03:39 -0700

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// Change the copyright
+const copyRight = document.getElementById('copyright'); // Auto Update Copyright
+const currentYear = new Date().getFullYear();
+copyRight.innerHTML = 'Copyright Amanda Watson ©' + currentYear;
